ae99f9c0be005b167b027edfbdce42bc8bb1866d
justinbthrift@gmail.com <justinthrift@Justins-MBP.home>
2019-12-03 20:17:08 -0500

    Assignment 13 First Attempt

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
 function onReady() {
-  const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
+  let id = 0;
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
 
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
-    event.preventDefault();
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
 
-    let title = NEW_TODO_TEXT.value;
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id++;
+    });
 
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
+    newToDoText.value = '';
+
+    renderTheUI;
+}
+
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo){
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
 
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
+    newLi.textContent = toDo.title;
 
-    deleteBtn.addEventListener('click', function(event){
-      TODO_LIST.removeChild(this.parentElement);
-    })
-
-    newLi.textContent = title;
+    toDoList.appendChild(newLi);
     newLi.appendChild(checkbox);
     newLi.appendChild(deleteBtn);
-    TODO_LIST.appendChild(newLi);
-    NEW_TODO_TEXT.value = "";
   });
-};
+}
+
+addToDoForm.addEventListener('submit', event => {
+  event.preventDefault();
+  createNewToDo();
+});
+
+
+const deleteBtn = document.createElement('button');
+      deleteBtn.textContent = "Delete";
+
+      deleteBtn.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+      }
+    };
+
+
+renderTheUI();
+}
 
 window.onload = function() {
   onReady();
